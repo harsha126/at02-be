e81a2b20afbdd2e7e5e256c2fb4ebd3aa5ab1c69
harsha126 <harsha.sdm111@gmail.com>
2023-11-11 16:39:53 +0530

    added db connection

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(express.json({limit:"50mb"}));
+app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json());
 let corsOptions = {
     origin: ["http://localhost:5550", "http://localhost:3000"],
@@ -13,6 +13,8 @@ let corsOptions = {
 app.use(cors());
 
 const mongoURL = "mongodb://localhost:27017/at02";
+const upstreamUrl =
+    "mongodb+srv://admin:5tvzRAqHfR3ujCIV@nodecluster.kufpkfs.mongodb.net/?retryWrites=true&w=majority";
 
 const port = 8000;
 app.listen(port, () => {
@@ -21,7 +23,7 @@ app.listen(port, () => {
 
 const mongoose = require("mongoose");
 mongoose
-    .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(upstreamUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB");
     })
